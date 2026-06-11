2fff0e20636e56d7b5f3fc8bdc3cb42f8533ee8b
MGDegenhardt <mgdegenhardt@gmail.com>
2026-06-11 19:29:18 -0300

    Second commit - adding classes to /code

---
+++ code/entityFactory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Player import Player
+
+
+class Entity Factory(Player):
+    def __init__(self):
+        pass
+
+    def get_entity(self, entity_type):
+        pass
